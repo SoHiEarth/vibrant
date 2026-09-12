a64238e5ecebbfd406800df80486ce2f38bf949e
SoHiEarth <126925211+SoHiEarth@users.noreply.github.com>
2026-09-13 03:54:07 +0900

    feat: Implement attributes and tags

--- include/light.h
+++ include/light.h
@@ -5,11 +5,3 @@ enum class LightType : int {
   Global,
   Point
 };
-
-struct Light {
-  LightType type = LightType::Point;
-  float intensity = 1;
-  glm::vec3 color = { 1.0f, 1.0f, 1.0f };
-  float radial_falloff = 1.0;
-  float volumetric_intensity = 1.0;
-};
